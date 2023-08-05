939001f09ccdf5f5bd692f858434a6e09ef1f876
Scotty <scottmtinez@gmail.com>
2023-08-04 21:13:13 -0500

    Add files via upload

---
+++ skills.js
@@ -0,0 +1,149 @@
+//Pops up window 
+    function open1(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s1').style.display = 'block';
+    }   
+
+    function open2(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s2').style.display = 'block';
+    }
+
+    function open3(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s3').style.display = 'block';
+    }
+
+    function open4(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s4').style.display = 'block';
+    }
+
+    function open5(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s5').style.display = 'block';
+    }
+
+    function open6(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s6').style.display = 'block';
+    }
+
+    function open7(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s7').style.display = 'block';
+    }
+
+    function open8(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s8').style.display = 'block';
+    }
+
+    function open9(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s9').style.display = 'block';
+    }
+
+    function open10(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s10').style.display = 'block';
+    }
+
+    function open11(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s11').style.display = 'block';
+    }
+
+    function open12(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s12').style.display = 'block';
+    }
+
+    function open13(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s13').style.display = 'block';
+    }
+
+    function open14(){
+        const d = document.getElementById('skbox').style.display = 'none';
+        const p = document.getElementById('s14').style.display = 'block';
+    }
+
+
+
+
+
+
+//Closes window
+    function close1(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s1').style.display = 'none';
+    }
+
+    function close2(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s2').style.display = 'none';
+    }
+
+    function close3(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s3').style.display = 'none';
+    }
+
+    function close4(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s4').style.display = 'none';
+    }
+    
+    function close5(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s5').style.display = 'none';
+    }
+
+    function close6(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s6').style.display = 'none';
+    }
+
+    function close7(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s7').style.display = 'none';
+    }
+
+    function close8(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s8').style.display = 'none';
+    }
+
+    function close9(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s9').style.display = 'none';
+    }
+
+    function close10(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s10').style.display = 'none';
+    }
+
+    function close11(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s11').style.display = 'none';
+    }
+
+    function close12(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s12').style.display = 'none';
+    }
+
+    function close13(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s13').style.display = 'none';
+    }
+
+    function close14(){
+        const d = document.getElementById('skbox').style.display = 'block';
+        const p = document.getElementById('s14').style.display = 'none';
+    }
+    
+    
+   
---
+++ skills.php
@@ -0,0 +1,312 @@
+<?php 
+    echo"<link rel='stylesheet' href='index.css'> ";
+    echo "<script src='skills.js'></script>";
+
+    echo "
+        <!-- C++ -->
+            <div class='sbox' id='s1'>
+                <a class='closelink' href='javascript:close1()'>X</a>
+                <h1>C++</h1>
+                <p>
+                    <h5> 4 Years of School Experience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    In school, I learned an advanced level of object-oriented programming, delved into intricate topics such as memory management, pointers, and user-defined data types. 
+                    The course also emphasized various aspects, such as reading and writing to files, dynamic arrays, and implementing the principles of object-oriented design, including encapsulation and inheritance. 
+                    Additionally, we focused on effective planning and testing techniques to ensure robust code. 
+                    Furthermore, we tackled the optimization of array sorting using various algorithms.<br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Sorting Arrays & Lists using Insertion Sort, Bubble Sort, Radix Sort, Merge Sort, Quick Sort, & Heap Sort. <br>
+                    Traversing through binary trees using DFS, BFS, & DAG. <br>
+                    Built a Tic Tac Toe computer game that the user would play against an AI system. <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Intro & Intermediate C++ <br>
+                    Data Structures <br>
+                    Theory of Algorithms <br>
+                    Intro to AI <br>
+                </p>
+            </div>
+
+        <!-- Java -->
+            <div class='sbox' id='s2'>
+                <a class='closelink' href='javascript:close2()'>X</a>
+                <h1>Java</h1>
+                <p>
+                    <h5>1 Year of School Experience</h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Advanced topics of Object-Oriented Programming. <br>
+                    Using algorithms to solve problems, and implement solutions with VS Code to employ objects and classes. <br>
+                    Taught how to use multi-dimensional arrays, methods, error handling, strings, regular expressions, encapsulation, inheritance, polymorphism, generic types, program debugging and testing, database and file processing, event-handling, and graphical user interfaces.<br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Using an array to print out a list of the best figure skaters from each state. <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Intro & Intermediate Java <br>
+                </p>
+            </div>
+
+        <!-- HTML -->
+            <div class='sbox' id='s3'>
+                <a class='closelink' href='javascript:close3()'>X</a>
+                <h1>HTML</h1>
+                <p>
+                    <h5> 5 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Managing windows, forms, events, & cookies using JavaScript. <br>
+                    Connecting HTML to a database. <br>
+                    Using HTML forms to update, delete, and create new information into the database. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Personal front-end portfolio Web Application <br>
+                    Group Project Recipe Web Application <br>
+                    Workout Routine Creator Web Application <br>
+                    Weather Widget that uses an API <br>
+                    Digital Clock Web Application <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    JavaScript & DHTML <br>
+                    Database Management Systems <br>
+                    Server-Side Scripting <br>
+                    Program Language Concepts <br>
+                    Web Server & UNIX Admin <br>
+                    Software Engineering <br>
+                    Advanced Web App Develement
+                </p>
+            </div>
+
+        <!-- CSS -->
+            <div class='sbox' id='s4'>
+                <a class='closelink' href='javascript:close4()'>X</a>
+                <h1>CSS</h1>
+                <p>
+                    <h5> 5 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Creating animations that will occur when an event happens like, when the users mouse hovers over a link or button is clicked. <br>
+                    Loading Animcations. <br>
+                    Modern like Web Application. <br>
+                    Responsive Web Application. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Personal front-end portfolio Web Application <br>
+                    Group Project Recipe Web Application <br>
+                    Workout Routine Creator Web Application <br>
+                    Weather Widget that uses an API <br>
+                    Digital Clock Web Application <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    JavaScript & DHTML <br>
+                    Database Management Systems <br>
+                    Server-Side Scripting <br>
+                    Program Language Concepts <br>
+                    Web Server & UNIX Admin <br>
+                    Software Engineering <br>
+                    Advanced Web App Develement
+                </p>
+            </div>
+
+        <!-- Python -->
+            <div class='sbox' id='s5'>
+                <a class='closelink' href='javascript:close5()'>X</a>
+                <h1>Python</h1>
+                <p>
+                    <h5> 1 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Creating structured programs that are based on simple algorithms that involve input, output, mathematical operations, decisions, and loops. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Calculator App <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Intro to Python Programming
+                </p>
+            </div>
+
+        <!-- Linux -->
+            <div class='sbox' id='s6'>
+                <a class='closelink' href='javascript:close6()'>X</a>
+                <h1>Python</h1>
+                <p>
+                    <h5> 1 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Implementing an intrusion detection and incident response system. <br>
+                    System Administrator. <br>
+                    Shell Programming Skills. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Adding programming projects to a file using Linux. <br>
+                    Running FIN_Scan and running the scan with Linux. <br>
+                    Checking to see if files have been updated by another computer without access. <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Intrusion & Detection <br>
+                    Digital Forensics <br>
+                    Cybersecurity Law <br>
+                    Web Server & UNIX Admin
+                </p>
+            </div>
+
+
+        <!-- Databases-->
+            <div class='sbox' id='s7'>
+                <a class='closelink' href='javascript:close7()'>X</a>
+                <h1>MySql, MariaDB, & MongoDB</h1>
+                <p>
+                    <h5> 3 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Using MySql to access, create, update, & delete data from a database. <br>
+                    Maintaining databases using PHP. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Personal front-end portfolio Web Application <br>
+                    Group Project Recipe Web Application <br>
+                    Workout Routine Creator Web Application <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Advanced Web App Development <br>
+                    Web Server & UNIX Admin <br>
+                    Software Engineering <br>
+                    Server-Side Scripting
+                </p>
+            </div>
+
+        <!-- PHP -->
+            <div class='sbox' id='s8'>
+                <a class='closelink' href='javascript:close8()'>X</a>
+                <h1>PHP</h1>
+                <p>
+                    <h5> 4 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Creating & managing dynamic web pages, databases, & session tracking. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Personal front-end portfolio Web Application <br>
+                    Group Project Recipe Web Application <br>
+                    Workout Routine Creator Web Application <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Advanced Web App Development <br>
+                    Web Server & UNIX Admin <br>
+                    Software Engineering <br>
+                    Server-Side Scripting
+                </p>
+            </div>
+
+        <!-- Bootstraps -->
+            <div class='sbox' id='s9'>
+                <a class='closelink' href='javascript:close9()'>X</a>
+                <h1>Bootstraps</h1>
+                <p>
+                    <h5> 3 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Using icons and navigations bars designed by Bootstraps. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Personal front-end portfolio Web Application <br>
+                    Group Project Recipe Web Application <br>
+                    Workout Routine Creator Web Application <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Advanced Web App Development <br>
+                    Software Engineering <br>
+                    Server-Side Scripting
+                </p>
+            </div>
+
+        <!-- Swift -->
+            <div class='sbox' id='s10'>
+                <a class='closelink' href='javascript:close10()'>X</a>
+                <h1>Swift</h1>
+                <p>
+                    <h5> 1 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Developing & creating mobile applications. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Temple Run like ios appication <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    1 App Development Course in High School
+                </p>
+            </div>
+
+        <!-- Adobe Photoshop-->
+            <div class='sbox' id='s11'>
+                <a class='closelink' href='javascript:close11()'>X</a>
+                <h1>Adobe Photoshop</h1>
+                <p>
+                    <h5> 5 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Desiging and printing art pieces and designs on to products like tops and bottoms through screen printing. <br>
+                    Editing pictures. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Editing Pictures <br>
+                    Social Media Profile Pictures <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Design for Digital Media <br> 
+                    4 Years of courses in high school & being a Teachers Aid
+                </p>
+            </div>
+
+        <!-- Adobe Illustrator -->
+            <div class='sbox' id='s12'>
+                <a class='closelink' href='javascript:close12()'>X</a>
+                <h1>Adobe Illustrator</h1>
+                <p>
+                    <h5> 5 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Designing Logos. <br>
+                    Desiging and printing art pieces and designs on to products like tops and bottoms through screen printing. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Business Logos <br>
+                    Creating designs to print out on products <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    Design for Digital Media <br> 
+                    4 Years of courses in high school & being a Teachers Aid
+                </p>
+            </div>
+        
+        <!-- JavaScript -->
+            <div class='sbox' id='s13'>
+                <a class='closelink' href='javascript:close13()'>X</a>
+                <h1>JavaScript</h1>
+                <p>
+                    <h5> 2.5 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Using JS to work with html when events happen. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Personal front-end portfolio Web Application <br>
+                    Weather Widget that uses an API <br>
+                    Digital Clock Web Application <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    JavaScript & DHTML <br>
+                    Self Teaching 
+                </p>
+            </div>
+
+        <!-- Node.js -->
+            <div class='sbox' id='s14'>
+                <a class='closelink' href='javascript:close14()'>X</a>
+                <h1>Node.js</h1>
+                <p>
+                    <h5> 1 Years of School Exerpience </h5>
+                    <h6 style='color: purple;'>Knowledge:</h6>
+                    Using Node.js to connect to databases and to scrape the web. <br><br>
+
+                    <h6 style='color: purple;'>Projects Worked On:</h6>
+                    Web Scraping <br><br>
+
+                    <h6 style='color: purple;'>Courses Taken:</h6>
+                    JavaScript & DHTML <br>
+                    Self Teaching 
+                </p>
+            </div>
+    ";
+?>
